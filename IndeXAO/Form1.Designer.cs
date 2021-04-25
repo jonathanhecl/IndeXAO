8e6d7032ae70c799a1fec1397cbfebb1410ccb5e
Jonathan Hecl <jonathanhecl@gmail.com>
2021-04-24 23:54:30 -0300

    initial commit

---
+++ IndeXAO/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace IndeXAO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGraficos = new System.Windows.Forms.TextBox();
+            this.cmdGraficos = new System.Windows.Forms.Button();
+            this.cmdIndice = new System.Windows.Forms.Button();
+            this.txtIndice = new System.Windows.Forms.TextBox();
+            this.lblGraficos = new System.Windows.Forms.Label();
+            this.listGraficos = new System.Windows.Forms.ListView();
+            this.lblIndice = new System.Windows.Forms.Label();
+            this.txtVer = new System.Windows.Forms.TextBox();
+            this.lstIndices = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // txtGraficos
+            // 
+            this.txtGraficos.Location = new System.Drawing.Point(184, 14);
+            this.txtGraficos.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGraficos.Name = "txtGraficos";
+            this.txtGraficos.ReadOnly = true;
+            this.txtGraficos.Size = new System.Drawing.Size(581, 35);
+            this.txtGraficos.TabIndex = 0;
+            // 
+            // cmdGraficos
+            // 
+            this.cmdGraficos.Location = new System.Drawing.Point(13, 14);
+            this.cmdGraficos.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdGraficos.Name = "cmdGraficos";
+            this.cmdGraficos.Size = new System.Drawing.Size(163, 37);
+            this.cmdGraficos.TabIndex = 1;
+            this.cmdGraficos.Text = "Dir. &Gráficos";
+            this.cmdGraficos.UseVisualStyleBackColor = true;
+            this.cmdGraficos.Click += new System.EventHandler(this.cmdGraficos_Click);
+            // 
+            // cmdIndice
+            // 
+            this.cmdIndice.Location = new System.Drawing.Point(14, 59);
+            this.cmdIndice.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdIndice.Name = "cmdIndice";
+            this.cmdIndice.Size = new System.Drawing.Size(163, 37);
+            this.cmdIndice.TabIndex = 3;
+            this.cmdIndice.Text = "&Ind. Gráficos";
+            this.cmdIndice.UseVisualStyleBackColor = true;
+            this.cmdIndice.Click += new System.EventHandler(this.cmdIndice_Click);
+            // 
+            // txtIndice
+            // 
+            this.txtIndice.Location = new System.Drawing.Point(185, 59);
+            this.txtIndice.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIndice.Name = "txtIndice";
+            this.txtIndice.ReadOnly = true;
+            this.txtIndice.Size = new System.Drawing.Size(580, 35);
+            this.txtIndice.TabIndex = 2;
+            // 
+            // lblGraficos
+            // 
+            this.lblGraficos.AutoSize = true;
+            this.lblGraficos.Location = new System.Drawing.Point(773, 18);
+            this.lblGraficos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGraficos.Name = "lblGraficos";
+            this.lblGraficos.Size = new System.Drawing.Size(110, 30);
+            this.lblGraficos.TabIndex = 4;
+            this.lblGraficos.Text = "Graficos: 0";
+            // 
+            // listGraficos
+            // 
+            this.listGraficos.AutoArrange = false;
+            this.listGraficos.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listGraficos.HideSelection = false;
+            this.listGraficos.LabelWrap = false;
+            this.listGraficos.Location = new System.Drawing.Point(734, 113);
+            this.listGraficos.Margin = new System.Windows.Forms.Padding(4);
+            this.listGraficos.MultiSelect = false;
+            this.listGraficos.Name = "listGraficos";
+            this.listGraficos.ShowGroups = false;
+            this.listGraficos.Size = new System.Drawing.Size(200, 406);
+            this.listGraficos.TabIndex = 5;
+            this.listGraficos.TileSize = new System.Drawing.Size(128, 32);
+            this.listGraficos.UseCompatibleStateImageBehavior = false;
+            this.listGraficos.View = System.Windows.Forms.View.Tile;
+            // 
+            // lblIndice
+            // 
+            this.lblIndice.AutoSize = true;
+            this.lblIndice.Location = new System.Drawing.Point(773, 62);
+            this.lblIndice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIndice.Name = "lblIndice";
+            this.lblIndice.Size = new System.Drawing.Size(100, 30);
+            this.lblIndice.TabIndex = 6;
+            this.lblIndice.Text = "Indices: 0";
+            // 
+            // txtVer
+            // 
+            this.txtVer.Location = new System.Drawing.Point(646, 113);
+            this.txtVer.Margin = new System.Windows.Forms.Padding(4);
+            this.txtVer.Name = "txtVer";
+            this.txtVer.Size = new System.Drawing.Size(81, 35);
+            this.txtVer.TabIndex = 7;
+            // 
+            // lstIndices
+            // 
+            this.lstIndices.AutoArrange = false;
+            this.lstIndices.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lstIndices.HideSelection = false;
+            this.lstIndices.LabelWrap = false;
+            this.lstIndices.Location = new System.Drawing.Point(15, 104);
+            this.lstIndices.Margin = new System.Windows.Forms.Padding(4);
+            this.lstIndices.MultiSelect = false;
+            this.lstIndices.Name = "lstIndices";
+            this.lstIndices.ShowGroups = false;
+            this.lstIndices.Size = new System.Drawing.Size(138, 406);
+            this.lstIndices.TabIndex = 8;
+            this.lstIndices.TileSize = new System.Drawing.Size(128, 32);
+            this.lstIndices.UseCompatibleStateImageBehavior = false;
+            this.lstIndices.View = System.Windows.Forms.View.Tile;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Controls.Add(this.lstIndices);
+            this.Controls.Add(this.txtVer);
+            this.Controls.Add(this.lblIndice);
+            this.Controls.Add(this.listGraficos);
+            this.Controls.Add(this.lblGraficos);
+            this.Controls.Add(this.cmdIndice);
+            this.Controls.Add(this.txtIndice);
+            this.Controls.Add(this.cmdGraficos);
+            this.Controls.Add(this.txtGraficos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IndeXAO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGraficos;
+        private System.Windows.Forms.Button cmdGraficos;
+        private System.Windows.Forms.Button cmdIndice;
+        private System.Windows.Forms.TextBox txtIndice;
+        private System.Windows.Forms.Label lblGraficos;
+        private System.Windows.Forms.ListView listGraficos;
+        private System.Windows.Forms.Label lblIndice;
+        private System.Windows.Forms.TextBox txtVer;
+        private System.Windows.Forms.ListView lstIndices;
+    }
+}
+
---
+++ IndeXAO/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Ini;
+
+namespace IndeXAO
+{
+    public partial class Form1 : Form
+    {
+
+        string configFile = "config.ini";
+        IConfigurationRoot ini;
+        string DirGraphics;
+        string FileIndex;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdGraficos_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                txtGraficos.Text = folderBrowserDialog.SelectedPath;
+                //ini.IniWriteValue("INIT", "DirGraphics", txtGraficos.Text);
+                string[] files = System.IO.Directory.GetFiles(folderBrowserDialog.SelectedPath);
+                int grh = 0;
+                //listGraficos.BeginUpdate();
+                listGraficos.Items.Clear();
+                foreach (string file in files)
+                {
+                    string name = System.IO.Path.GetFileName(file);
+                    if (name.EndsWith(".bmp") || name.EndsWith(".png"))
+                    {
+                        string first = System.IO.Path.GetFileNameWithoutExtension(name);
+                        bool isNumeric = int.TryParse(first, out _);
+                        if (isNumeric) {
+                            listGraficos.Items.Add(name);
+                            grh++;
+                        }
+                    }
+                }
+                //listGraficos.EndUpdate();
+                lblGraficos.Text = "Gráficos: " + grh.ToString();
+                /*
+                if (File.Exists(Path.Combine(folderBrowserDialog.SelectedPath, "archivo.ini")))
+                {
+                    label7.Text = "Carpeta válida";
+                }
+                else
+                {
+                    label7.Text = "Carpeta invalida";
+                }
+                */
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ini = new ConfigurationBuilder().
+            //    SetBasePath(System.IO.Directory.GetCurrentDirectory()).
+            //    AddIniFile(configFile).Build();
+            //txtGraficos.Text = ini.IniReadValue("INIT", "DirGraficos");
+        }
+
+        private void cmdIndice_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Abrir indice";
+            openFileDialog.DefaultExt = "";
+            openFileDialog.FileName = "";
+            openFileDialog.Filter = "Archivo de indice (*.ini;*.ind)|*.ini;*.ind|Todos los archivos (*.*)|*.*";
+            openFileDialog.FilterIndex = 0;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtIndice.Text = openFileDialog.FileName;
+                CargarIndice(txtIndice.Text);
+            }
+
+        }
+
+        System.IO.MemoryStream fileMemory = new System.IO.MemoryStream();
+
+        public void CargarIndice(string filepath)
+        {
+            string name = System.IO.Path.GetFileName(filepath);
+            if (name.EndsWith(".ind")) {
+                System.IO.FileInfo fi = new System.IO.FileInfo(filepath);
+                lblIndice.Text = fi.Length.ToString();
+                fileMemory.Dispose();
+                fileMemory = new System.IO.MemoryStream();
+                using (System.IO.FileStream fs = System.IO.File.OpenRead(filepath))
+                {
+                    fs.CopyTo(fileMemory);
+                }
+
+                fileMemory.Seek(0x00, System.IO.SeekOrigin.Begin);
+
+                // Version
+                var ver = new byte[4];
+                fileMemory.Read(ver, 0, (int)ver.Length);
+                txtVer.Text = BitConverter.ToInt32(ver).ToString();
+
+                // MaxGrh
+                var maxGrh = new byte[4];
+                fileMemory.Read(maxGrh, 0, (int)maxGrh.Length);
+                lblIndice.Text = "Indices: " + BitConverter.ToInt32(maxGrh).ToString();
+
+                lstIndices.BeginUpdate();
+                lstIndices.Items.Clear();
+                for (int i = 1; i <= BitConverter.ToInt32(maxGrh); i++)
+                {
+                    lstIndices.Items.Add(i.ToString());
+                }
+                lstIndices.EndUpdate();
+
+
+                bool exit = false;
+                do
+                {
+                    // Grh number
+                    var number = new byte[4];
+                    fileMemory.Read(number, 0, (int)number.Length);
+
+                    // Frames
+                    var frames = new byte[2];
+                    fileMemory.Read(frames, 0, (int)frames.Length);
+
+                    if (BitConverter.ToInt16(frames) == 1)
+                    {
+                        var image = new byte[4];
+                        fileMemory.Read(image, 0, (int)image.Length);
+                        
+                        var posX = new byte[2];
+                        fileMemory.Read(posX, 0, (int)posX.Length);
+                        var posY = new byte[2];
+                        fileMemory.Read(posY, 0, (int)posY.Length);
+                        var ancho = new byte[2];
+                        fileMemory.Read(ancho, 0, (int)ancho.Length);
+                        var alto = new byte[2];
+                        fileMemory.Read(alto, 0, (int)alto.Length);
+
+                        /*
+                        lstIndices.Items.Add("Grh" + BitConverter.ToInt32(number).ToString() +
+                            "=" + BitConverter.ToInt16(frames).ToString() + "-"+ 
+                            BitConverter.ToInt32(image).ToString() + "-" +
+                            BitConverter.ToInt16(posX).ToString() + "-" +
+                            BitConverter.ToInt16(posY).ToString() + "-" +
+                            BitConverter.ToInt16(ancho).ToString() + "-" +
+                            BitConverter.ToInt16(alto).ToString());
+                        */
+                    }
+                    else if (BitConverter.ToInt16(frames) > 1)
+                    {
+                        short maxFrames = BitConverter.ToInt16(frames);
+                        string tempStr = "";
+                        do
+                        {
+                            var grh = new byte[4];
+                            fileMemory.Read(grh, 0, (int)grh.Length);
+                            tempStr = tempStr + "-" + BitConverter.ToInt32(grh).ToString();
+                            maxFrames--;
+                        } while (maxFrames > 0);
+
+                        var speed = new byte[4];
+                        fileMemory.Read(speed, 0, (int)speed.Length);
+
+                        /*
+                        lstIndices.Items.Add("Grh" + BitConverter.ToInt32(number).ToString() +
+                            "=" + BitConverter.ToInt16(frames).ToString() + 
+                            tempStr + "-" + BitConverter.ToSingle(speed).ToString());
+                        */
+                    } else
+                    {
+                        exit = true;
+                    }
+                } while (!exit || fileMemory.Position > fileMemory.Length);
+
+            } else if (name.EndsWith(".ini"))
+            {
+                MessageBox.Show("Es indice ini");
+            } else
+            {
+                //
+            }
+        }
+    }
+}
